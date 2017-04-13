301d3a43f464e0316d780324b9a6074dc10d7098
Silvio Donato <silvio.donato@cern.ch>
2017-04-13 10:01:47 +0200

    add Count histo in merge and skim .C

--- mergeAndSkim.C
+++ mergeAndSkim.C
@@ -2,6 +2,7 @@
 #include"TChain.h"
 #include"TTree.h"
 #include"TFile.h"
+#include"TH1F.h"
 #include <iostream>
 
 void skimAndMergeFile(TString inputFiles, TString outputFile){
@@ -10,7 +11,8 @@ void skimAndMergeFile(TString inputFiles, TString outputFile){
     tree->Add(inputFiles);
 
     fileout->cd();
-    if(tree->GetEntries()<=0) {
+    int entries = tree->GetEntries();
+    if(entries<=0) {
         cout<<"No entries in file "<<inputFiles<<endl;
         throw 1;
     }
@@ -21,11 +23,17 @@ void skimAndMergeFile(TString inputFiles, TString outputFile){
         }
     }
     newTree->Write();
+
+    /// Add histogram with Count ///
+    TH1F* Count = new TH1F("Count","Count",1,0,1);
+    Count->SetBinContent(1,entries);
+    Count->Write();
 }
 
 void mergeAndSkim(){
-    TString inputFiles = TString("root://storage01.lcg.cscs.ch//pnfs/lcg.cscs.ch/cms/trivcat/store/user/sdonato/HLT_Ntuple_Hbb_Signal_v3_noAODSIM/QCD_Pt_30to50_TuneCUETP8M1_13TeV_pythia8/HLT_Ntuple_Hbb_Signal_v3_noAODSIM/170408_101635/0000/tree_11*.root");
-    TString outputFile = TString("QCD30to50.root");
-    skimAndMergeFile(inputFiles, outputFile);
+    skimAndMergeFile(
+        "root://storage01.lcg.cscs.ch//pnfs/lcg.cscs.ch/cms/trivcat/store/user/sdonato/HLT_Ntuple_Hbb_Signal_v3_noAODSIM/QCD_Pt_30to50_TuneCUETP8M1_13TeV_pythia8/HLT_Ntuple_Hbb_Signal_v3_noAODSIM/170408_101635/0000/tree_11*.root", 
+        "QCD30to50.root"
+        );
 }
 

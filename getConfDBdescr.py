ad2032559d1816b50afb78298f114c1a0c16e6fe
Silvio <silvio.donato@cern.ch>
2017-06-02 11:56:28 +0200

    add code to make https://twiki.cern.ch/twiki/bin/view/CMS/ConfDB910

---
+++ getConfDBdescr.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+import cx_Oracle
+import sys
+
+def getConfDBdescr(configBase):
+    ## Connect to confDB
+    connstr = 'cms_hlt_gdr_r/convertMe!@cmsr'
+    conn = cx_Oracle.connect(connstr)
+    curs = conn.cursor()
+    curs.arraysize=50
+
+    confDBdescr = []
+    ## Loop on HLT versions
+    for version in range(1,300):
+        configuration = configBase + "/V"+str(version)
+        query="select a.description from u_confversions a where a.name='"+configuration+"'"
+        curs.execute(query)
+        curs_copy=curs
+        ## get first row (curs_copy contains only one element here)
+        for rows in curs_copy:
+            ## get description, if it is not empty
+            try:
+                description = rows[0].read()
+            except:
+                description =""
+            
+            ## add link to JIRA tickets
+            posInit = description.find("CMSHLT-")
+            if posInit>=0:
+                posFinal = posInit+7
+                while (posFinal<len(description) and description[posFinal].isdigit()): posFinal+=1
+                cmshlt = description[posInit:posFinal]
+                description = description.replace(cmshlt,"[[https://its.cern.ch/jira/browse/%s][%s]]"%(cmshlt,cmshlt))
+             
+            ## add '!' in front of capital letters
+            for i in range(len(description)):
+                if description[i].isupper() and (i==0 or description[i-1]==" "):
+                    description = description[:i]+"!"+description[i:]
+            
+            ## fill confDBdescr
+            confDBdescr.append((configuration,description))
+    
+    confDBdescr.reverse()
+    return confDBdescr
+
+if __name__ == "__main__":
+    confDBdescr = getConfDBdescr(sys.argv[1])
+    print confDBdescr
---
+++ makeConfDBXXXTWiki.py
@@ -0,0 +1,21 @@
+### This code is used to generate the TWiki such as https://twiki.cern.ch/twiki/bin/view/CMS/ConfDB900
+
+import sys
+
+condDBbase = sys.argv[1]
+
+from getConfDBdescr import getConfDBdescr
+confDBdescr = getConfDBdescr(condDBbase)
+
+### Loop in releases
+twiki ="""
+---+ !HLT menus in !ConfDB: pp menus for "Run2" in 2016
+
+
+---++ !HLT Developments in !ConfDB %s
+"""%(condDBbase)
+for (configuration, description) in confDBdescr:
+    ### download the html and parse it
+        twiki += "   * =%s=: %s\n"%(configuration,description)
+
+print twiki

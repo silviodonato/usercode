3597e1a378715410b5ba78d9b0eb146dc3eec765
Silvio Donato <silvio.donato@cern.ch>
2018-06-07 09:26:29 +0200

    update

--- plots_from_RooFit_WorkSpace_RooStat.py
+++ plots_from_RooFit_WorkSpace_RooStat.py
@@ -1,3 +1,5 @@
+# https://root.cern.ch/download/doc/RooFit_Users_Manual_2.91-33.pdf
+
 import ROOT
 
 c1 = ROOT.TCanvas("c1")
@@ -11,7 +13,10 @@ print()
 data = w.data("data_obs")
 bkg = w.data("CaloTrijet2016_qq_mjj")
 sig = w.data("CaloTrijet2016_qq")
-sig_JESup = w.data("CaloTrijet2016_qq_jesUp")
+sig_JES_up = w.data("CaloTrijet2016_qq_jesUp")
+sig_JES_down = w.data("CaloTrijet2016_qq_jesDown")
+sig_JER_up = w.data("CaloTrijet2016_qq_jerUp")
+sig_JER_down = w.data("CaloTrijet2016_qq_jerDown")
 
 
 mjj = w.var("mjj")
@@ -25,7 +30,13 @@ frame = th1x.frame()
 
 
 #data.plotOn(frame)
-sig_JESup.plotOn(frame)
-#sig.plotOn(frame)
-frame.Draw()
+sig.plotOn(frame)
 
+#sig_JES_up.plotOn(    frame,ROOT.RooFit.LineColor(ROOT.kRed) , ROOT.RooFit.DataError(ROOT.RooAbsData.None))
+#sig_JES_down.plotOn(  frame,ROOT.RooFit.LineColor(ROOT.kBlue) , ROOT.RooFit.DataError(ROOT.RooAbsData.None))
+
+sig_JER_up.plotOn(    frame,ROOT.RooFit.LineColor(ROOT.kRed), ROOT.RooFit.DataError(ROOT.RooAbsData.None))
+sig_JER_down.plotOn(  frame,ROOT.RooFit.LineColor(ROOT.kBlue), ROOT.RooFit.DataError(ROOT.RooAbsData.None))
+
+#sig.plotOn(frame)
+frame.Draw("")

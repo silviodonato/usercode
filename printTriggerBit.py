98278cb87587eb502e27eccbf1eda49ac8fcbf82
Silvio Donato <silvio.donato@cern.ch>
2021-10-26 17:00:46 +0200

    Move to python3, add a loop on events

--- printTriggerBit.py
+++ printTriggerBit.py
@@ -12,10 +12,10 @@ def checkTriggerIndex(name,index, names):
         firstTriggerError = True
     if index>=names.size():
         if firstTriggerError:
-            for tr in names: print tr
-            print
-            print name," not found!"
-            print
+            for tr in names: print(tr)
+            print()
+            print()
+            print(name," not found!")
             firstTriggerError = False
             return False
         else:
@@ -32,7 +32,40 @@ triggerNames = names.triggerNames()
 for triggerName in triggerNames:
     index = names.triggerIndex(triggerName)
     if checkTriggerIndex(triggerName,index,names.triggerNames()):
-        print triggerBits.product().accept(index),
+        print(triggerBits.product().accept(index)),
     else:
-        print "-1",
-    print triggerName
+        print("-1",)
+    print(triggerName)
+
+
+####################
+
+l1tBits, l1tBitLabel = Handle("GlobalObjectMapRecord"), ("hltGtStage2ObjectMap::%s"%HLTprocess)
+event.getByLabel(l1tBitLabel, l1tBits)
+gtMap = l1tBits.product().gtObjectMap()
+for obj in gtMap:
+    print(obj.algoBitNumber(), obj.algoName(), obj.algoGtlResult())
+
+
+######################3
+for iev,event in enumerate(events):
+    try:
+        event.getByLabel(l1tBitLabel, l1tBits)
+        l1tBits.product()
+#        print("OK")
+    except:
+#        print("NO L1T MAP")
+        continue
+    event.getByLabel(triggerBitLabel, triggerBits)
+    triggerBits.product()
+#    names = event.object().triggerNames(triggerBits.product())
+#    index = names.triggerIndex("HLT_Random_v3")
+    index = names.triggerIndex("HLT_Physics_v7")
+    if triggerBits.product().accept(index):
+#        print("Trigger Accepted")
+        ZeroBias = l1tBits.product().getObjectMap("L1_ZeroBias")
+        ETT35 = l1tBits.product().getObjectMap("L1_ETT35")
+        ETT20 = l1tBits.product().getObjectMap("L1_ETT20")
+        print(ZeroBias.algoGtlResult(), ETT35.algoGtlResult(), ETT20.algoGtlResult())
+
+    

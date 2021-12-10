e22c71ca0469ffecd93f602961fad59724c378ee
Silvio Donato <silvio.donato@cern.ch>
2021-12-10 19:59:25 +0100

    update

--- triggerObjects_MINIAOD_MET_resolution.py
+++ triggerObjects_MINIAOD_MET_resolution.py
@@ -1,10 +1,15 @@
 trigger="MC_PFMET_v17"
 metCollection="hltPFMETOpenFilter"
 from DataFormats.FWLite import Handle, Events
-offMETs, offMETLabel = Handle("vector<pat::MET>"), ("slimmedMETs")
+#offMETs, offMETLabel = Handle("vector<pat::MET>"), ("slimmedMETs")
+offMETs, offMETLabel = Handle("vector<pat::MET>"), ("slimmedMETsPuppi")
 
 #xrdcp root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18MiniAODv2/VBF_HToInvisible_M125_TuneCP5_withDipoleRecoil_13TeV_powheg_pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/70000/3ADE2E10-FD8F-B941-9C62-400308C99FB8.root .
-fileName='3ADE2E10-FD8F-B941-9C62-400308C99FB8.root'
+#fileName='3ADE2E10-FD8F-B941-9C62-400308C99FB8.root'
+
+fileName = "190C6D7C-FC26-A949-BE2C-551947B33215.root"
+#fileName = ["/store/mc/RunIISummer20UL18MiniAODv2/ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/00000/190C6D7C-FC26-A949-BE2C-551947B33215.root"]
+
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
@@ -18,23 +23,25 @@ ROOT.AutoLibraryLoader.enable()
 c1 = ROOT.TCanvas("c1")
 
 offMETvsGen = ROOT.TH2F("offMETvsGen","",100,0.,400.,100,-200.,200.)
-offMETvsGen.GetYaxis().SetTitle("offline/hlt MET")
+offMETvsGen.GetYaxis().SetTitle("MET - genMET")
 offMETvsGen.GetXaxis().SetTitle("gen MET")
 offMETvsGen.SetLineWidth(2)
 offMETvsGen.SetMarkerSize(2)
 
 hltMETvsGen = offMETvsGen.Clone("hltMETvsGen")
 
-hltMETvsOff = offMETvsGen.Clone("hltMETvsGen")
+test = offMETvsGen.Clone("hltMETvsGen")
 
 
-offMETRes = ROOT.TH1F("offMETRes","",100,-1.,1.)
+offMETRes = ROOT.TH1F("offMETRes","",100,-100.,100)
 offMETRes.GetXaxis().SetTitle("offline/hlt MET")
 offMETRes.GetYaxis().SetTitle("Events")
 offMETRes.SetLineWidth(2)
 offMETRes.SetMarkerSize(2)
 
 hltMETRes = offMETRes.Clone("hltMETRes")
+hltoffMETRes = offMETRes.Clone("hltoffMETRes")
+testMETRes = offMETRes.Clone("testMETRes")
 
 
 # load FWlite python libraries
@@ -65,24 +72,36 @@ for iev,event in enumerate(events):
             to.unpackFilterLabels(event.object(), triggerBits.product())
             if metCollection in to.filterLabels():
                 hltMET = to.pt()
-        if genMET>100 and genMET<150:
-            offMETRes.Fill( (offlineMET - genMET)/genMET )
-            hltMETRes.Fill( (hltMET - genMET)/genMET )
+        if genMET>50:
+            offMETRes.Fill( (offlineMET - genMET) )
+            hltMETRes.Fill( (hltMET - genMET) )
+            hltoffMETRes.Fill( (hltMET - offlineMET) )
+            testMETRes.Fill( (hltMET + offlineMET)/2 - genMET )
         hltMETvsGen.Fill( genMET, hltMET - genMET )
         offMETvsGen.Fill( genMET, offlineMET - genMET )
-        hltMETvsOff.Fill( offlineMET, hltMET - offlineMET )
+        test.Fill( genMET, (hltMET + offlineMET)/2 - genMET )
     
 
 c1 = ROOT.TCanvas("c1")
 
 offMETRes.SetLineColor(ROOT.kRed)
 hltMETRes.SetLineColor(ROOT.kBlue)
+hltoffMETRes.SetLineColor(ROOT.kBlack)
+testMETRes.SetLineColor(ROOT.kGreen)
+offMETRes.Fit("gaus")
+hltMETRes.Fit("gaus")
+hltoffMETRes.Fit("gaus")
+testMETRes.Fit("gaus")
 offMETRes.Draw()
 hltMETRes.Draw("same")
+#hltoffMETRes.Draw("same")
+testMETRes.Draw("same")
 
 leg = ROOT.TLegend(0.1,0.7,0.28,0.9)
 leg.AddEntry(offMETRes,"offline","lep")
 leg.AddEntry(hltMETRes,"HLT","lep")
+leg.AddEntry(testMETRes,"(HLT+offline)/2","lep")
+leg.AddEntry(hltoffMETRes,"HLT vs offline","lep")
 leg.Draw()
 
 c1.SaveAs("resolution.root")
@@ -93,15 +112,36 @@ c2 = ROOT.TCanvas("c2")
 
 offMETvsGenProf = offMETvsGen.ProfileX("_off",1,-1,"s")
 hltMETvsGenProf = hltMETvsGen.ProfileX("_hlt",1,-1,"s")
-hltMETvsOffProf = hltMETvsOff.ProfileX("_hltvsoff",1,-1,"s")
+testProf = test.ProfileX("_hltvsoff",1,-1,"s")
 offMETvsGenProf.SetLineColor(ROOT.kRed)
 hltMETvsGenProf.SetLineColor(ROOT.kBlue)
+testProf.SetLineColor(ROOT.kGreen)
+
+offMETvsGenProf.SetMaximum( 50)
+offMETvsGenProf.SetMinimum(-50)
+offMETvsGenProf.Draw()
+c2.SaveAs("offline.png")
+hltMETvsGenProf.SetMaximum( 50)
+hltMETvsGenProf.SetMinimum(-50)
+hltMETvsGenProf.Draw()
+c2.SaveAs("HLT.png")
+testProf.SetMaximum( 50)
+testProf.SetMinimum(-50)
+testProf.Draw()
+c2.SaveAs("test.png")
+
+offMETvsGenProf.Draw()
+hltMETvsGenProf.Draw()
+testProf.Draw()
+
 offMETvsGenProf.Draw()
 hltMETvsGenProf.Draw("same")
+testProf.Draw("same")
 
 leg2 = ROOT.TLegend(0.1,0.7,0.28,0.9)
 leg2.AddEntry(offMETvsGenProf,"offline","lep")
 leg2.AddEntry(hltMETvsGenProf,"HLT","lep")
+leg2.AddEntry(testMETRes,"(HLT+offline)/2","lep")
 leg2.Draw()
 
 
@@ -110,7 +150,7 @@ c2.SaveAs("2Dplot.png")
 
 c3 = ROOT.TCanvas("c3")
 
-hltMETvsOffProf.Draw()
+testProf.Draw()
 
 c3.SaveAs("hltVsOff.root")
 c3.SaveAs("hltVsOff.png")

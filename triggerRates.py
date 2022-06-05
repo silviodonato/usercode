5c7372365b0b6af0b74bb5aecf34eeda7e92749c
Silvio <silvio.donato@cern.ch>
2022-06-05 15:10:31 +0200

    update to the new MessageLogger

--- triggerRates.py
+++ triggerRates.py
@@ -4,7 +4,7 @@ process = cms.Process("TRIGREP")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(30)
+    input = cms.untracked.int32(5000)
 )
 
 process.options = cms.untracked.PSet(
@@ -13,7 +13,7 @@ process.options = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-       'file:trigRes_1.root'
+       '/store/express/Run2022A/ExpressPhysics/FEVT/Express-v1/000/352/572/00000/b5dd76fd-07de-4a93-a786-6bf83677a8cf.root'
     ),
 )
 
@@ -23,8 +23,22 @@ process.hltTrigReport = cms.EDAnalyzer( "HLTrigReport",
     serviceBy = cms.untracked.string( "never" ),
     resetBy = cms.untracked.string( "never" ),
     reportBy = cms.untracked.string( "job" ),
-    HLTriggerResults = cms.InputTag( 'TriggerResults','','MYHLT' )
+    HLTriggerResults = cms.InputTag( 'TriggerResults','','HLT' )
 )
 
-process.TrigR = cms.Path(process.hltTrigReport)
-process.MessageLogger.categories.append("HLTrigReport")
+process.hltTrigReportRECO = cms.EDAnalyzer( "HLTrigReport",
+    ReferencePath = cms.untracked.string( "HLTriggerFinalPath" ),
+    ReferenceRate = cms.untracked.double( 100.0 ),
+    serviceBy = cms.untracked.string( "never" ),
+    resetBy = cms.untracked.string( "never" ),
+    reportBy = cms.untracked.string( "job" ),
+    HLTriggerResults = cms.InputTag( 'TriggerResults','','RECO' )
+)
+
+process.TrigR = cms.Path(process.hltTrigReport) # + process.hltTrigReportRECO)
+
+
+process.MessageLogger.cerr.HLTrigReport = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000),
+            reportEvery = cms.untracked.int32(1)
+)
